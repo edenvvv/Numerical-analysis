4044a23d3429164cf321239302fed97656456dd6
edenvvv <edenvvv@gmail.com>
2020-10-21 20:57:35 +0300

    blob

--- lab_1/lab 1.py
+++ lab_1/lab 1.py
@@ -1,12 +1,13 @@
 import math
 import sys
 # 1
+ε = sys.float_info.epsilon
 num1 = float(input("enter the a (in a*(b/c-d)-e): "))
 num2 = float(input("enter the b (in a*(b/c-d)-e): "))
 num3 = float(input("enter the c (in a*(b/c-d)-e): "))
 num4 = int(input("enter the d (in a*(b/c-d)-e): "))
 num5 = int(input("enter the e (in a*(b/c-d)-e): "))
-print(math.fabs(num1*(num2/num3-num4)-num5)-sys.float_info.epsilon)
+print(math.fabs(num1*(num2/num3-num4)-num5)-ε)
 
 # 2.1
 print(f"Machine precision is: {sys.float_info.epsilon}")
