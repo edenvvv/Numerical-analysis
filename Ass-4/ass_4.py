08d8f593b7bcf19d676179703594c80dae1181f1
edenvvv <edenvvv@gmail.com>
2020-12-02 18:07:07 +0200

    .

--- Ass-4/ass_4.py
+++ Ass-4/ass_4.py
@@ -1,3 +1,7 @@
+import numpy
+from numpy import log as ln
+
+
 # Defining Function
 def f(x):
     return x ** 4 + x ** 3 - 3*x ** 2
@@ -6,31 +10,34 @@ def f(x):
 # Implementing Bisection Method
 def bisection_method(a, b, e):
     # add check error blob
-    step = 1
-    condition = True
-    while condition:
-        m = (a + b) / 2
-        print('Iteration-%d, m = %0.6f and f(m) = %0.6f' % (step, m, f(m)))
+    if f(a) * f(b) > 0.0:
+        print('Try Again with different guess values')
+    else:
+        stop = - ln(e/(b-a))/ln(2)
+        step = 1
+        condition = True
+        while condition:
+            if step > stop:
+                print("The method is not suitable")
+                exit(0)
+            m = (a + b) / 2
+            print('Iteration-%d, m = %0.6f and f(m) = %0.6f' % (step, m, f(m)))
 
-        if f(a) * f(m) < 0:
-            b = m
-        else:
-            a = m
+            if f(a) * f(m) < 0:
+                b = m
+            else:
+                a = m
 
-        step += 1
-        condition = abs(f(m)) > e
+            step += 1
+            condition = abs(f(m)) > e
 
-    print('\nRequired Root is : %0.5f' % m)
+        print('\nRequired Root is : %0.5f' % m)
 
 
-# Input Section
+# main
 a = float(input('First Guess: '))
 b = float(input('Second Guess: '))
 e = 0.0001
 
 # Checking Correctness of initial guess values and bisecting
-if f(a) * f(b) > 0.0:
-    print('Given guess values do not bracket the root.')
-    print('Try Again with different guess values.')
-else:
-    bisection_method(a, b, e)
+bisection_method(a, b, e)
