a4ddbe6bf1c271455a5a2eb2cd9acb6feac92755
edenvvv <edenvvv@gmail.com>
2020-12-03 15:34:46 +0200

    .

--- Ass-4/ass_4.py
+++ Ass-4/ass_4.py
@@ -1,43 +1,78 @@
-import numpy
-from numpy import log as ln
+# eden dadon id:207279183
+# Eliran Dagan id: 208061580
+import sympy as sp
 
 
-# Defining Function
 def f(x):
-    return x ** 4 + x ** 3 - 3*x ** 2
+    return x**4 + x**3 - 3*x**2
 
 
-# Implementing Bisection Method
-def bisection_method(a, b, e):
+def derivative_of_f(x):
+    return 4*x**3 + 3*x**2 - 6*x
+
+
+def bisection_method(a, b, e, func):
     # add check error blob
-    if f(a) * f(b) > 0.0:
+    if func(a) * func(b) > 0.0:
         print('Try Again with different guess values')
     else:
-        stop = - ln(e/(b-a))/ln(2)
         step = 1
         condition = True
         while condition:
-            if step > stop:
-                print("The method is not suitable")
-                exit(0)
             m = (a + b) / 2
-            print('Iteration-%d, m = %0.6f and f(m) = %0.6f' % (step, m, f(m)))
 
-            if f(a) * f(m) < 0:
+            if func(a) * func(m) < 0:
                 b = m
             else:
                 a = m
 
             step += 1
-            condition = abs(f(m)) > e
+            condition = abs(func(m)) > e
 
-        print('\nRequired Root is : %0.5f' % m)
+        if func.__name__ == "derivative_of_f":
+            if f(round(m, 1)) == 0.0:
+                result.append(m)
+        else:
+            result.append(m)
+
+
+def max_range(min_range, max_range):
+    segment = 0.1
+    segment_list = []
+    index = min_range
+
+    while index <= max_range:
+        segment_list.append(round(index, 1))
+        index += segment
+    segment_list.append(max_range)
+
+    for inx, x in enumerate(segment_list):
+        if inx < len(segment_list) - 1:
+
+            if segment_list[inx+1] == 0:
+                if inx < len(segment_list) - 2:
+
+                    if f(x) * f(segment_list[inx + 2]) < 0.00:
+                        bisection_method(x, segment_list[inx + 2], e, f)
+
+                    if derivative_of_f(x) * derivative_of_f(segment_list[inx + 2]) < 0.00:
+                        bisection_method(x, segment_list[inx + 2], e, derivative_of_f)
+
+            if f(x) * f(segment_list[inx + 1]) < 0.00:
+                bisection_method(x, segment_list[inx + 1], e, f)
+
+            if derivative_of_f(x) * derivative_of_f(segment_list[inx + 1]) < 0.00:
+                bisection_method(x, segment_list[inx + 1], e, derivative_of_f)
 
 
 # main
 a = float(input('First Guess: '))
 b = float(input('Second Guess: '))
-e = 0.0001
+result = []
+e = 0.00001
 
-# Checking Correctness of initial guess values and bisecting
-bisection_method(a, b, e)
+max_range(a, b)
+print("there roots are:")
+
+for i in result:
+    print("%0.5f" % i)
